46d81d1e4bb38d387a102bcd1fadabed9afaeebb
e_erikm <e_erikm@sebraesp.com.br>
2023-05-17 11:22:24 -0300

    começo do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 export function Header(){
     return(
-        <div>
-            menu
+        <div className="flex flex-row">
+            <div className="flex-grow">
+                <button>AKIO</button>
+            </div>
+            <div className="bg-slate-400 ml-auto">
+                <button>NISSAN</button>
+                <button>TOYOTA</button>
+                <button>MITSUBISHI</button>
+                <button>SUBARU</button>
+                <button>MAZDA</button>
+                <button>HONDA</button>
+            </div>
         </div>
     )
 }
